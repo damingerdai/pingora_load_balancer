2ea0cce3d1f402cc38cb4237c7492f3b7c42b93d
Arthur Ming <guobin.ming@pwc.com>
2024-03-12 15:01:18 +0800

    feat: health check

--- src/main.rs
+++ src/main.rs
@@ -44,8 +44,16 @@ fn main() {
     let mut my_server = Server::new(None).unwrap();
     my_server.bootstrap();
 
-    let upstreams = LoadBalancer::try_from_iter(["1.1.1.1:443", "1.0.0.0:443"]).unwrap();
-    let mut lb = http_proxy_service(&my_server.configuration, LB(Arc::new(upstreams)));
+    let mut upstreams = LoadBalancer::try_from_iter(["1.1.1.1:443", "1.0.0.0:443"]).unwrap();
+
+    let hc = TcpHealthCheck::new();
+    upstreams.set_health_check(hc);
+    upstreams.health_check_frequency = Some(std::time::Duration::from_secs(1));
+
+    let background = background_service("health check", upstreams);
+    let upstreams = background.task();
+
+    let mut lb = http_proxy_service(&my_server.configuration, LB(upstreams));
     lb.add_tcp("0.0.0.0:6188");
 
     my_server.add_service(lb);
